7fbbec80890b5fd888a7deaf11b016e5a3089536
stas khass <stas.khass@test.ru>
2023-12-06 06:40:01 +0300

    add ex9.5 about users . Working with acces to variables in class

---
+++ class/users2.py
@@ -0,0 +1,36 @@
+#Упражнение 9.5 Работа с системным пользователем или с пользователем в сиситеме
+class Users():
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print(f" Имя пользователя {self.first_name}")
+        print(f" Фамилия пользователя {self.last_name}")
+        print(f" Количество попыток входа пользователя {self.login_attempts}")
+        print("---")
+
+    def greet_user(self):
+        print(f" Привет {self.first_name}, {self.last_name}. Добро пожаловать в систему.")
+
+    def increment_login_attempts(self):
+        self.login_attempts = self.login_attempts + 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
+user = Users("Stas", "Khass")
+user.describe_user()
+
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+
+user.describe_user()
+user.reset_login_attempts()
+
+user.describe_user()
