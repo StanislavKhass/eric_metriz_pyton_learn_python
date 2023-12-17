c3860378069bf2c05cf9c8dfe43ca03d9740d13e
stas khass <stas.khass@test.ru>
2023-12-17 10:37:29 +0300

    finish ex9.9 in clases in oop

--- class/cars/cars.py
+++ class/cars/cars.py
@@ -36,15 +36,32 @@ class ElectricCar(Cars):
     #Атрибуты специфические для автомобиля
     def __init__(self, make , model , year):
         super().__init__(make , model , year)
-        self.battery_size = 75
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        #У электро мобиля нет бензобака
+        print("This car doesn't need a gas tank!")
+
+class Battery():
+    # Тут описываем все что связанно с аккамулятором
+    def __init__(self, battery_size=75):
+            self.battery_size = battery_size
 
     def describe_battery(self):
         #Выводим информацию о мощьости аккамулятрра
         print(f"This car has a {self.battery_size}-kWh battery")
 
-    def fill_gas_tank(self):
-        #У электро мобиля нет бензобака
-        print("This car doesn't need a gas tank!")
+    def get_range(self):
+        #Выводит приблизительный запас хода для аккамулятора
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge")
+
+    def upgrade_battery(self):
+        if self.battery_size != 100:
+            self.battery_size = 100
 
 my_new_car = Cars('Audi', 'a4', '2019')
 print(my_new_car.get_discriptive_name())
@@ -67,5 +84,10 @@ my_used_car.read_odometer()
 
 my_tesla = ElectricCar("Testla", "model s", 2019 )
 print(my_tesla.get_discriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
 my_tesla.fill_gas_tank()
+
+print("==== задание 9.9 ===")
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
